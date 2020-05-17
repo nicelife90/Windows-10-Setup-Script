af3a149c4e4d0b60f826d9546b456163d090746e
oZ-Zo <50652175+oz-zo@users.noreply.github.com>
2020-05-18 00:12:08 +0300

    Power App initial commit :tada:

---
+++ Power Apps/Controls/HamburgerIconButton.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Power_App.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для HamburgerIconButton.xaml
+    /// </summary>
+    public partial class HamburgerIconButton : UserControl
+    {
+        public HamburgerIconButton()
+        {
+            InitializeComponent();
+        }
+        
+        public string Icon
+        {
+            get { return (string)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(string), typeof(HamburgerIconButton), new PropertyMetadata(default(string)));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(HamburgerIconButton), new PropertyMetadata(default(string)));
+    }
+}
